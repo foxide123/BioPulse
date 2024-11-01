8a28058ea247c89ca716f942b35baecec6bac409
Mihail Bolfa <bolfamihail@gmail.com>
2024-11-02 00:37:14 +0100

    defined a DbContext class for database

---
+++ dotnet/Common/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace dotnet.Common.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<SensorRaedings> SensorReadings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=hydroponicsystem.db");
+        }
+
+    }
+}
